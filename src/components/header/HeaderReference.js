563c7c514830100c16c37e535b7106417b28fc74
lakshmi-789 <139106808+lakshmi-789@users.noreply.github.com>
2023-07-18 09:57:00 +0530

    name changes

--- src/components/header/HeaderReference.js
+++ src/components/header/HeaderReference.js
@@ -7,7 +7,7 @@ class Header extends Component {
   constructor(props) {
     super(props);
     this.userData = {
-      name: "Veerendra Kumar",
+      name: "Rajyalakshmi",
       age: "29",
     };
 
